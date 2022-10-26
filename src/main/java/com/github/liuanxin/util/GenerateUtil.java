f14001a9ed38d058143b674bfa9be69aec624de2
Jie <liu_anxin@163.com>
2022-10-26 10:56:34 +0800

    fix

--- src/main/java/com/github/liuanxin/util/GenerateUtil.java
+++ src/main/java/com/github/liuanxin/util/GenerateUtil.java
@@ -1,9 +1,7 @@
 package com.github.liuanxin.util;
 
 import java.math.BigDecimal;
-import java.util.Arrays;
 import java.util.Date;
-import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.concurrent.atomic.AtomicLong;
 
@@ -15,24 +13,39 @@ public class GenerateUtil {
 
     private static final String CHAR = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
-    private static final List<String> NAMES = Arrays.asList(
-            "林木", "孔剑", "邓舷", "邓龄", "许涛", "于山", "乔恒", "邹航", "龚海", "张拯",
-            "李心水", "曾雨华", "谭高义", "杜鸿光", "蔡正谊", "文德元", "张明旭", "龙修平", "贺德运", "马展鹏",
-            "赵冬", "苏俞", "高娣", "丁咏", "田凤", "程荷", "丁璇", "潘斐", "冯水", "张婉",
-            "孔晓芬", "常冰之", "罗文茵", "钟柔惠", "丁卿云", "李芸莹", "钟雪柳", "钱文英", "乔思萌", "孔海萍"
-    );
+    private static final String SUR_NAME = "赵钱孙李周吴郑王冯陈杨朱秦尤许何吕施张孔曹严华谢邹章云苏潘范彭林孔邓韦昌马许于乔马高";
+    private static final String GIRL_NAME = "秀娟英华慧巧美娜静淑惠珠翠雅芝玉萍红娥玲芬芳燕彩春菊兰凤洁梅琳素云莲真环雪荣爱妹霞香月媛艳凡佳嘉琼勤珍莉桂娣妍茜秋珊莎锦羽希宁欣育柔竹亚宜枝思丽";
+    private static final String BOY_NAME = "伟刚勇毅俊峰强军平东文辉力明永健世广志义兴良海山仁波宁贵福龙元全国胜学祥发武新利承乐绍功松善厚庆朋斌梁栋维启克伦翔旭鹏泽晨辰士以建家致树德行时泰";
 
-    private static final List<String> SHOP_NAMES = Arrays.asList(
-            "谷之商铺", "帝狼商铺", "红世峰商铺", "燃点商铺", "零距离食品城", "亿廊商铺",
-            "艾贝特服装", "天阳商铺", "牛转乾坤商铺", "奥威通华商铺", "光艺商铺", "亮化商铺"
-    );
+    private static final String[] TEL_ARR = "3,5,8".split(",");
+    private static final String[] EMAIL_SUFFIX = "@gmail.com,@msn.com,@live.com,@qq.com,@163.com,@126.com".split(",");
 
-    public static String toName() {
-        return NAMES.get(RANDOM.nextInt(NAMES.size()));
+    public static String toPhone() {
+        return "1" + TEL_ARR[RANDOM.nextInt(TEL_ARR.length)] + RANDOM.nextInt(10)
+                + String.format("%04d", RANDOM.nextInt(10000))
+                + String.format("%04d", RANDOM.nextInt(10000));
     }
 
-    public static String toShop() {
-        return SHOP_NAMES.get(RANDOM.nextInt(SHOP_NAMES.size()));
+    public static String toEmail() {
+        StringBuilder sbd = new StringBuilder();
+        int length = RANDOM.nextInt(6, 18);
+        for (int i = 0; i < length; i++) {
+            sbd.append(CHAR.charAt(RANDOM.nextInt(CHAR.length())));
+        }
+        sbd.append(EMAIL_SUFFIX[RANDOM.nextInt(EMAIL_SUFFIX.length)]);
+        return sbd.toString();
+    }
+
+    public static String toName() {
+        StringBuilder sbd = new StringBuilder();
+        sbd.append(SUR_NAME.charAt(RANDOM.nextInt(SUR_NAME.length())));
+        boolean male = RANDOM.nextBoolean();
+        int num = RANDOM.nextInt(2) + 1;
+        for (int i = 0; i < num; i++) {
+            String name = (male ? BOY_NAME : GIRL_NAME);
+            sbd.append(name.charAt(RANDOM.nextInt(name.length())));
+        }
+        return sbd.toString();
     }
 
     public static String toCode(String prefix) {
--- src/main/java/com/github/liuanxin/web/TableColumnController.java
+++ src/main/java/com/github/liuanxin/web/TableColumnController.java
@@ -44,6 +44,7 @@ public class TableColumnController {
                                 String name = tableColumn.getAlias();
                                 Object info;
                                 Class<?> fieldType = tableColumn.getFieldType();
+                                String lowerCase = name.toLowerCase().replace("_", "");
                                 if (name.equals(tableInfo.getLogicColumn())) {
                                     info = tableInfo.getLogicValue();
                                 } else if (fieldType == Boolean.class) {
@@ -53,25 +54,24 @@ public class TableColumnController {
                                 } else if (fieldType == Long.class) {
                                     info = GenerateUtil.toLong(100000000L);
                                 } else if (fieldType == BigDecimal.class) {
-                                    info = GenerateUtil.toDecimal(10000D);
-                                } else {
-                                    String lowerCase = name.toLowerCase();
-                                    if (fieldType == Date.class) {
-                                        if ("createtime".equals(lowerCase) || "updatetime".equals(lowerCase)) {
-                                            info = null;
-                                        } else {
-                                            info = GenerateUtil.toDate(30);
-                                        }
+                                    info = GenerateUtil.toDecimal(10000000D);
+                                } else if (fieldType == Date.class) {
+                                    if ("createtime".equals(lowerCase)) {
+                                        info = GenerateUtil.toDate(120);
+                                    } else if ("updatetime".equals(lowerCase)) {
+                                        info = GenerateUtil.toDate(10);
                                     } else {
-                                        if (lowerCase.contains("shop")) {
-                                            info = GenerateUtil.toShop();
-                                        } else if (lowerCase.endsWith("name")) {
-                                            info = GenerateUtil.toName();
-                                        } else if (lowerCase.endsWith("code")) {
-                                            info = GenerateUtil.toCode(lowerCase.substring(0, lowerCase.indexOf("code")));
-                                        } else {
-                                            info = GenerateUtil.toVarchar(10);
-                                        }
+                                        info = GenerateUtil.toDate(30);
+                                    }
+                                } else {
+                                    if (lowerCase.contains("username")) {
+                                        info = GenerateUtil.toName();
+                                    } else if (lowerCase.contains("phone") || lowerCase.contains("tel")) {
+                                        info = GenerateUtil.toPhone();
+                                    } else if (lowerCase.contains("email")) {
+                                        info = GenerateUtil.toEmail();
+                                    } else {
+                                        info = GenerateUtil.toVarchar(10);
                                     }
                                 }
                                 data.put(name, info);
