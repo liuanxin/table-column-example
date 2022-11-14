2d7b980c345c581d383c88e6d4bbf0a219704f84
Jie <liu_anxin@163.com>
2022-11-14 17:29:09 +0800

    fix

--- src/test/java/com/github/example/IdTest.java
+++ src/test/java/com/github/example/IdTest.java
@@ -25,34 +25,29 @@ public class IdTest {
     private static final ZoneId ZONE_ID = ZoneId.systemDefault();
 
     public static void main(String[] args) throws Exception {
+        System.out.println("------------------");
+        for (int i = 0; i < 20; i++) {
+            System.out.println(IdUtil.getId());
+        }
+        System.out.println("------------------");
+
         long start = System.currentTimeMillis();
         System.out.println("start : " + FORMATTER.format(LocalDateTime.ofInstant(new Date(start).toInstant(), ZONE_ID)));
 
-        List<Future<Long>> callList = new ArrayList<>();
+        List<Future<Long>> futureList = new ArrayList<>();
         for (int i = 0; i < COUNT; i++) {
-            callList.add(THREAD_POOL.submit(IdUtil::getId));
+            futureList.add(THREAD_POOL.submit(IdUtil::getId));
         }
+
         Set<Long> set = ConcurrentHashMap.newKeySet();
-        for (Future<Long> future : callList) {
+        for (Future<Long> future : futureList) {
             set.add(future.get());
         }
         long end = System.currentTimeMillis();
-        System.out.printf("time  : %sms\n", ((end - start) / 1000.0));
+        System.out.println("time  : " + (end - start) + "ms");
         System.out.println("end   : " + FORMATTER.format(LocalDateTime.ofInstant(new Date(end).toInstant(), ZONE_ID)));
         System.out.println("------------------");
         System.out.println("all   : " + COUNT);
         System.out.println("real  : " + set.size());
-
-        int i = 0;
-        for (Long s : set) {
-            if (i >= 20) {
-                return;
-            }
-            i++;
-            System.out.println(s);
-        }
-        List<Runnable> runnableList = THREAD_POOL.shutdownNow();
-        System.out.println("------------------");
-        System.out.println(runnableList.size());
     }
 }
