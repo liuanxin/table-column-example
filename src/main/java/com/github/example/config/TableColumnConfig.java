a9911f5fa59f824461ba4abaecf4836ae01f1a9a
TY <liu.anxin13@gmail.com>
2023-02-10 10:13:27 +0800

    fix

--- src/main/java/com/github/example/config/TableColumnConfig.java
+++ src/main/java/com/github/example/config/TableColumnConfig.java
@@ -26,12 +26,19 @@ public class TableColumnConfig {
     public Map<String, ReqAlias> queryAliasMap() {
         return Map.of(
                 "all-order", new ReqAlias("Order"),
+                "order-address-item-log", orderAddressItemLogAlias()
+        );
+    }
 
-                "order-address-item-log", new ReqAlias("Order", new ReqResult(List.of(
-                        "orderNo", "orderStatus", "amount", "desc", "createTime",
-                        Map.of("address", Map.of("table", "OrderAddress", "columns", List.of("contact", "phone", "address"))),
-                        Map.of("items", Map.of("table", "OrderItem", "columns", List.of("productName", "price", "number"))),
-                        Map.of("logs", Map.of("table", "OrderLog", "columns", List.of("operator", "message", "time")))
-                ))));
+    private ReqAlias orderAddressItemLogAlias() {
+        // 指定 订单表 的字段, 指定 订单地址表 订单项表 订单日志表 的字段, 查询 Order 时 distinct, 并指定查询时多张表之间的关联方式
+        ReqResult result = new ReqResult(List.of(
+                "orderNo", "orderStatus", "amount", "desc", "createTime",
+                Map.of("address", Map.of("table", "OrderAddress", "columns", List.of("contact", "phone", "address"))),
+                Map.of("items", Map.of("table", "OrderItem", "columns", List.of("productName", "price", "number"))),
+                Map.of("logs", Map.of("table", "OrderLog", "columns", List.of("operator", "message", "time")))
+        ), true);
+        List<List<String>> relationList = List.of(List.of("Order", "inner", "OrderAddress"), List.of("Order", "left", "OrderItem"));
+        return new ReqAlias("Order", result, relationList);
     }
 }
