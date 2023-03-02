0a8e697afa0235f5bad230f0a66d9b3077098361
TY <liu.anxin13@gmail.com>
2023-03-02 23:13:41 +0800

    fix

--- src/main/java/com/github/example/web/TableColumnController.java
+++ src/main/java/com/github/example/web/TableColumnController.java
@@ -18,8 +18,6 @@ import java.util.List;
 @RequiredArgsConstructor
 public class TableColumnController {
 
-//    private static final TypeReference<List<Map<String, Object>>> TYPE_REFERENCE = new TypeReference<List<Map<String, Object>>>() {};
-
     private final TableColumnTemplate tableColumnTemplate;
 
     @GetMapping("/table-column-refresh")
@@ -102,77 +100,4 @@ public class TableColumnController {
         */
         return tableColumnTemplate.dynamicQuery(req);
     }
-
-//    @GetMapping("/generate-user")
-//    @ApiMethod(value = "生成数据", index = 4)
-//    public String generate(@ApiParam("表名") String tables, @ApiParam("生成个数") Integer count) {
-//        int flag = 0;
-//        if (tables != null && !tables.trim().isEmpty() && count != null && count > 0) {
-//            for (String table : tables.split(",")) {
-//                table = table.trim();
-//                Table tableInfo = tableColumnTemplate.getTcInfo().findTable(table);
-//                if (tableInfo != null) {
-//                    List<Map<String, Object>> dataList = new ArrayList<>();
-//                    for (int i = 0; i < count; i++) {
-//                        Map<String, Object> data = new HashMap<>();
-//                        List<String> idKeyList = tableInfo.getIdKey();
-//                        for (TableColumn tableColumn : tableInfo.getColumnMap().values()) {
-//                            if (!idKeyList.contains(tableColumn.getName())) {
-//                                String name = tableColumn.getAlias();
-//                                Object info;
-//                                Class<?> fieldType = tableColumn.getFieldType();
-//                                String lowerCase = name.toLowerCase().replace("_", "");
-//                                if (name.equals(tableInfo.getLogicColumn())) {
-//                                    info = tableInfo.getLogicValue();
-//                                } else if (fieldType == Boolean.class) {
-//                                    info = GenerateUtil.toBoolean();
-//                                } else if (fieldType == Integer.class) {
-//                                    info = GenerateUtil.toInt(10);
-//                                } else if (fieldType == Long.class) {
-//                                    info = GenerateUtil.toLong(30000L);
-//                                } else if (fieldType == BigDecimal.class) {
-//                                    info = GenerateUtil.toDecimal(50000D, 2);
-//                                } else if (fieldType == Date.class) {
-//                                    if ("createtime".equals(lowerCase)) {
-//                                        info = GenerateUtil.toDate(120);
-//                                    } else if ("updatetime".equals(lowerCase)) {
-//                                        info = GenerateUtil.toDate(10);
-//                                    } else {
-//                                        info = GenerateUtil.toDate(150);
-//                                    }
-//                                } else {
-//                                    if (lowerCase.contains("username")) {
-//                                        info = GenerateUtil.toName();
-//                                    } else if (lowerCase.contains("phone") || lowerCase.contains("tel")) {
-//                                        info = GenerateUtil.toPhone();
-//                                    } else if (lowerCase.contains("email")) {
-//                                        info = GenerateUtil.toEmail();
-//                                    } else if (lowerCase.contains("code")) {
-//                                        info = GenerateUtil.toCode(null);
-//                                    } else {
-//                                        info = GenerateUtil.toVarchar(10);
-//                                    }
-//                                }
-//                                data.put(name, info);
-//                            }
-//                        }
-//                        if (QueryUtil.isNotEmpty(data)) {
-//                            dataList.add(data);
-//                        }
-//                    }
-//                    flag += tableColumnTemplate.insertBatch(table, dataList);
-//                }
-//            }
-//        }
-//        return "生成成功 " + flag + " 条";
-//    }
-//
-//    @PostMapping("/generate-user")
-//    @ApiMethod(value = "直接生成数据", index = 5)
-//    public String generate(@RequestBody Map<String, Object> req) {
-//        String table = QueryUtil.toStr(req.get("table"));
-//        List<Map<String, Object>> dataList = QueryJsonUtil.convertType(req.get("data"), TYPE_REFERENCE);
-//        int flag = tableColumnTemplate.insertBatch(table, dataList);
-//        return "成功 " + flag + " 条";
-//    }
 }
