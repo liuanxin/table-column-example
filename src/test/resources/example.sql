347f7cab80d9cd93b7c606d0058ee09950a2cf85
TY <liu.anxin13@gmail.com>
2023-11-21 18:58:00 +0800

    fix rebuild

--- src/test/resources/example.sql
+++ src/test/resources/example.sql
@@ -1,74 +1,77 @@
 
-DROP DATABASE IF EXISTS `table-column-example`;
-CREATE DATABASE `table-column-example`;
-USE `table-column-example`;
+drop database if exists `table-column-example`;
+create database `table-column-example`;
+use `table-column-example`;
 
 
-DROP TABLE IF EXISTS `t_order`;
-CREATE TABLE `t_order` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `order_no` varchar(32) NOT NULL COMMENT '订单号',
-  `order_status` int unsigned NOT NULL DEFAULT '0' COMMENT '订单状态(0.用户已创建待支付, 1.用户已支付待商户发货, 2.商户已发货待用户签收, 3.用户已签收待确认完结, 4.已完结)',
-  `amount` decimal(20,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '订单金额',
-  `desc` varchar(32) NOT NULL DEFAULT '' COMMENT '备注',
-  `create_time` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
-  `update_time` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
-  `deleted` int unsigned NOT NULL DEFAULT '0' COMMENT '0.未删除, 非 0.已删除',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_order_no` (`order_no`,`deleted`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='订单';
+drop table if exists `t_order`;
+create table `t_order` (
+  `id` bigint unsigned not null auto_increment,
+  `order_no` varchar(32) not null comment '订单号',
+  `order_status` int unsigned not null default '0' comment '订单状态(0.用户已创建待支付, 1.用户已支付待商户发货, 2.商户已发货待用户签收, 3.用户已签收待确认完结, 4.已完结)',
+  `amount` decimal(20,2) unsigned not null default '0.00' comment '订单金额',
+  `desc` varchar(32) not null default '' comment '备注',
+  `create_time` datetime(6) not null default current_timestamp(6) comment '创建时间',
+  `update_time` datetime(6) not null default current_timestamp(6) on update current_timestamp(6) comment '更新时间',
+  `deleted` int unsigned not null default '0' comment '0.未删除, 非 0.已删除',
+  primary key (`id`),
+  unique key `uk_order_no` (`order_no`,`deleted`)
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci comment='订单';
 
-INSERT INTO `t_order` (`order_no`, `order_status`, `amount`, `desc`) VALUES
-('1-2-221010-00001', 1, 9258.00, '请尽快发货'), ('1-2-221010-00002', 2, 6900.00, '');
-
-
-DROP TABLE IF EXISTS `t_order_address`;
-CREATE TABLE `t_order_address` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `order_no` varchar(32) NOT NULL DEFAULT '' COMMENT '订单号',
-  `contact` varchar(16) NOT NULL DEFAULT '' COMMENT '联系人',
-  `phone` varchar(16) NOT NULL DEFAULT '' COMMENT '联系电话',
-  `address` varchar(128) NOT NULL DEFAULT '' COMMENT '联系人地址',
-  `create_time` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
-  `update_time` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) ON UPDATE CURRENT_TIMESTAMP(6) COMMENT '更新时间',
-  `deleted` bigint unsigned NOT NULL DEFAULT '0' COMMENT '0.未删除, 非 0.已删除',
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `uk_order_no` (`order_no`,`deleted`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='订单地址';
-
-INSERT INTO `t_order_address` (`order_no`, `contact`, `phone`, `address`) VALUES
-('1-2-221010-00001', '张三', '13012345678', '北京市朝阳区'), ('1-2-221010-00002', '李四', '13122223333', '广东省广州市白云区');
+insert into `t_order` (`order_no`, `order_status`, `amount`, `desc`) values
+('1-2-221010-00001', 1, 9258.00, '请尽快发货'),
+('1-2-221010-00002', 2, 6900.00, '');
 
 
-DROP TABLE IF EXISTS `t_order_item`;
-CREATE TABLE `t_order_item` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `order_no` varchar(32) NOT NULL DEFAULT '' COMMENT '订单号',
-  `product_name` varchar(32) NOT NULL DEFAULT '' COMMENT '商品名',
-  `price` decimal(20,2) unsigned NOT NULL DEFAULT '0.00' COMMENT '商品价格',
-  `number` int unsigned NOT NULL DEFAULT '0' COMMENT '商品数量',
-  PRIMARY KEY (`id`),
-  KEY `idx_order_no` (`order_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='订单项(商品)';
+drop table if exists `t_order_address`;
+create table `t_order_address` (
+  `id` bigint unsigned not null auto_increment,
+  `order_no` varchar(32) not null default '' comment '订单号',
+  `contact` varchar(16) not null default '' comment '联系人',
+  `phone` varchar(16) not null default '' comment '联系电话',
+  `address` varchar(128) not null default '' comment '联系人地址',
+  `create_time` datetime(6) not null default current_timestamp(6) comment '创建时间',
+  `update_time` datetime(6) not null default current_timestamp(6) on update current_timestamp(6) comment '更新时间',
+  `deleted` bigint unsigned not null default '0' comment '0.未删除, 非 0.已删除',
+  primary key (`id`),
+  unique key `uk_order_no` (`order_no`,`deleted`)
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci comment='订单地址';
 
-INSERT INTO `t_order_item` (`order_no`, `product_name`, `price`, `number`) VALUES
-('1-2-221010-00001', 'iPhone 14 Pro', 8999, 1), ('1-2-221010-00001', '女款外套', 129.5, 2),
-('1-2-221010-00002', 'Samsung', 6999.00, 1);
+insert into `t_order_address` (`order_no`, `contact`, `phone`, `address`) values
+('1-2-221010-00001', '张三', '13012345678', '北京市朝阳区'),
+('1-2-221010-00002', '李四', '13122223333', '广东省广州市白云区');
 
 
-DROP TABLE IF EXISTS `t_order_log`;
-CREATE TABLE `t_order_log` (
-  `id` bigint unsigned NOT NULL AUTO_INCREMENT,
-  `order_no` varchar(32) NOT NULL DEFAULT '' COMMENT '订单号',
-  `operator` varchar(32) NOT NULL DEFAULT '' COMMENT '操作人',
-  `message` text COMMENT '操作内容',
-  `time` datetime(6) NOT NULL DEFAULT CURRENT_TIMESTAMP(6) COMMENT '创建时间',
-  `deleted` tinyint(1) NOT NULL DEFAULT '0' COMMENT '0.未删除, 1.已删除',
-  PRIMARY KEY (`id`),
-  KEY `idx_order_no` (`order_no`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci COMMENT='订单日志';
+drop table if exists `t_order_item`;
+create table `t_order_item` (
+  `id` bigint unsigned not null auto_increment,
+  `order_no` varchar(32) not null default '' comment '订单号',
+  `product_name` varchar(32) not null default '' comment '商品名',
+  `price` decimal(20,2) unsigned not null default '0.00' comment '商品价格',
+  `number` int unsigned not null default '0' comment '商品数量',
+  primary key (`id`),
+  key `idx_order_no` (`order_no`)
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci comment='订单项(商品)';
 
-INSERT INTO `t_order_log` (`order_no`, `operator`, `message`, `time`) VALUES
+insert into `t_order_item` (`order_no`, `product_name`, `price`, `number`) values
+('1-2-221010-00001', 'iphone 14 pro', 8999, 1),
+('1-2-221010-00001', '女款外套', 129.5, 2),
+('1-2-221010-00002', 'samsung', 6999.00, 1);
+
+
+drop table if exists `t_order_log`;
+create table `t_order_log` (
+  `id` bigint unsigned not null auto_increment,
+  `order_no` varchar(32) not null default '' comment '订单号',
+  `operator` varchar(32) not null default '' comment '操作人',
+  `message` text comment '操作内容',
+  `time` datetime(6) not null default current_timestamp(6) comment '创建时间',
+  `deleted` tinyint(1) not null default '0' comment '0.未删除, 1.已删除',
+  primary key (`id`),
+  key `idx_order_no` (`order_no`)
+) engine=InnoDB default charset=utf8mb4 collate=utf8mb4_unicode_ci comment='订单日志';
+
+insert into `t_order_log` (`order_no`, `operator`, `message`, `time`) values
 ('1-2-221010-00001', '张三', '创建订单', '2022-10-01 11:12:13'),
 ('1-2-221010-00001', '张三', '支付成功', '2022-10-01 12:13:14'),
 ('1-2-221010-00002', '李四', '创建订单', '2022-10-01 12:15:14'),
