ff40385f7d7292c8b1dfd42bc7f239cdba7c74b6
TY <liu.anxin13@gmail.com>
2024-06-16 21:44:17 +0800

    fix

--- src/main/java/com/github/example/web/TableColumnController.java
+++ src/main/java/com/github/example/web/TableColumnController.java
@@ -11,14 +11,14 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@ApiGroup("表字段")
+@ApiGroup("业务查增改删")
 @RestController
 @RequiredArgsConstructor
 public class TableColumnController {
 
     private final TableColumnTemplate tableColumnTemplate;
 
-    @PostMapping("/table-column")
+    @PostMapping("/table-column-refresh")
     @ApiMethod(value = "刷新结构", index = 0)
     public Boolean refresh() {
         return tableColumnTemplate.refreshWithDatabase();
@@ -30,7 +30,7 @@ public class TableColumnController {
         return tableColumnTemplate.info(tables);
     }
 
-    @PostMapping("/query")
+    @PostMapping("/table-column")
     @ApiMethod( value = "数据查询", index = 2)
     public Object query(@RequestBody ReqInfo req) {
         /*
@@ -80,11 +80,11 @@ public class TableColumnController {
         return tableColumnTemplate.dynamicQuery(req);
     }
 
-    @PostMapping("/query-{alias}")
-    @ApiMethod( value = "别名查询", index = 3)
+    @PostMapping("/table-column/{alias}")
+    @ApiMethod( value = "数据查询(基于别名)", index = 3)
     public Object aliasQuery(@PathVariable("alias") String alias, @RequestBody ReqAlias req) {
         /*
-        POST /query-order-address-item-log
+        POST /table-column/order-address-item-log
         {
           "req" : {
             "query" : {
@@ -97,5 +97,70 @@ public class TableColumnController {
         }
         */
         return tableColumnTemplate.dynamicQuery(alias, req);
+    }
+
+    @PostMapping("/table-column-add")
+    @ApiMethod( value = "数据添加", index = 4)
+    public Object addTable(@RequestBody Object obj) {
+        /*
+        POST /table-column-add
+        {
+            "Order" : {
+                "order_no": "订单号",
+                "amount": "100.00",
+                "desc": "备注1",
+                "OrderAddress": {
+                }
+                "OrderItem": [{
+                },{
+                }]
+            }
+        }
+
+
+        POST /table-column-add
+        {
+            "Order" : [{
+                "order_no": "订单号",
+                "amount": "200.00",
+                "desc": "备注2",
+                "OrderAddress": {
+                }
+                "OrderItem": [{
+                },{
+                }]
+            }]
+        }
+        */
+        return tableColumnTemplate.insertData(obj);
+    }
+
+    @PostMapping("/table-column-update")
+    @ApiMethod( value = "数据更新", index = 5)
+    public Object updateTable(@RequestBody Object obj) {
+        /*
+        POST /table-column/update
+        {
+            "Order" : {
+                "id": "123",
+                "desc": "备注"
+            }
+        }
+        */
+        return tableColumnTemplate.updateData(obj);
+    }
+
+    @PostMapping("/table-column-del")
+    @ApiMethod( value = "数据删除", index = 6)
+    public Object delTable(@RequestBody Object obj) {
+        /*
+        POST /table-column/del
+        {
+            "Order" : {
+                "id": "123"
+            }
+        }
+        */
+        return tableColumnTemplate.deleteData(obj);
     }
 }
